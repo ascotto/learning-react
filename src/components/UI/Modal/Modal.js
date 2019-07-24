39d2621da7799528272e06eb27f19c72518572b4
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2019-07-24 23:58:57 +0200

    Modal component part 2

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,20 @@
 import React from "react";
 import classes from "./Modal.module.css";
+import Aux from "../../../hoc/Auxiliary";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
-const modal = props => <div className={classes.Modal}>{props.children}</div>;
+const modal = props => (
+    <Aux>
+        <div className={classes.Modal}
+            style={{
+                transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+                opacity: props.show ? "1" : "0"
+            }}
+        >
+            {props.children}
+        </div>
+        <Backdrop show={props.show} clicked={props.modalClosed}></Backdrop>
+    </Aux>
+);
 
 export default modal;
