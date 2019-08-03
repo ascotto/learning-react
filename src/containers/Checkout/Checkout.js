3f3d76c9fd3152132b8aae56b63d10eb125637e6
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2019-08-03 13:02:15 +0200

    Checkout Summary

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,21 +1,60 @@
 import React, { Component } from "react";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummray";
+import { Route } from "react-router-dom";
+import CheckoutData from "../Checkout/ContactData/ContactData";
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      cheese: 2,
-      meat: 1,
-      bacon: 2,
-      onion: 1
+    ingredients: null,
+    price: 0
+  };
+
+  componentWillMount() {
+    console.log(this.props);
+    const query = new URLSearchParams(this.props.location.search);
+
+    const ingredients = {};
+    let price = 0;
+
+    for (let param of query.entries()) {
+      if (param[0] === "price") {
+        price = param[1];
+      } else {
+        ingredients[param[0]] = +param[1];
+      }
     }
+
+    this.setState({
+      ingredients: ingredients,
+      totalPrice: price
+    });
+  }
+
+  checkoutCancelHandler = () => {
+    this.props.history.goBack();
+  };
+  checkoutContinueHandler = () => {
+    this.props.history.replace("/checkout/contact-data");
   };
 
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancel={this.checkoutCancelHandler}
+          checkoutContinue={this.checkoutContinueHandler}
+        />
+        Contact Data
+        <Route
+          path={this.props.match.path + "/contact-data"}
+          render={() => (
+            <CheckoutData
+              ingredients={this.state.ingredients}
+              price={this.state.totalPrice}
+            />
+          )}
+        />
       </div>
     );
   }
