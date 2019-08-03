61501c731ebe0e49e1dbf56e22c500d710602ac7
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2019-08-03 10:41:45 +0200

    Adding React Dom Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
+import { Route, Switch } from "react-router-dom";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 
@@ -8,8 +9,10 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
